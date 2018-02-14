0fb73179163c867b6979e74f37db0446d69d0502
Ryan Struthers <ryan.s.struthers@gmail.com>
2018-02-14 11:26:07 -0700

    added script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//$(document).ready(function(){
+//	console.log("document is ready to roll!");
+//	function init_map(){
+//		console.log("testing");
+//	}
+//	$.ajax(
+//		var api_key = 'AIzaSyDw2skiQuAeamtbcyay1eYlHfu87B1xH8M';
+//		var api_url = "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&callback=initMap";
+//	});
+//});
+//
+
+$(document).ready(function(){
+	console.log("document is ready to rock and roll!");
+});
